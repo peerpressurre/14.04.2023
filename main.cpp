07696cbd9ba0642d163e922775273708b2bb61e0
peerpressurre <gsophiay@gmail.com>
2023-04-05 23:17:38 +0300

    yay

---
+++ main.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+using std::cout;
+using std::istream;
+using std::ostream;
+using std::fstream;
+using std::ios_base;
+#define next_line { cout << endl; }
+
+struct File
+{
+private:
+    string path;
+    fstream file;
+    ios_base::open_mode open_mode;
+public:
+    //Setters & Getters
+    void setPath(string path)
+    {
+        this->path = path;
+    }
+
+    void setOpenMode(ios_base::open_mode open_mode)
+    {
+        this->open_mode = open_mode;
+    }
+
+    ios_base::open_mode getOpenMode()
+    {
+        return open_mode;
+    }
+
+    string getPath()
+    {
+        return path;
+    }
+
+
+    //Constructors & Destructor 
+    File()
+    {
+        setPath("text.txt");
+        setOpenMode(ios::out);
+    }
+    File(string path) : File()
+    {
+        setPath(path);
+    }
+    File(string path, ios_base::open_mode open_mode) : File(path)
+    {
+        setOpenMode(open_mode);
+    }
+    ~File()
+    {
+        path.clear();
+        file.close();
+        open_mode = 0;
+    }
+    //Actions 
+    bool Write(string text, bool append = false) {
+        if (open_mode != ios::out && append == false)
+        {
+            open_mode = ios::out;
+        }
+        else if (open_mode != ios::app && append == true)
+        {
+            open_mode = ios::app;
+        }
+        file.open(path, open_mode);
+        if (file.is_open())
+        {
+            file << text << endl;
+            this->CloseFile();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool Load(string& text) {
+        string buf;
+        if (open_mode != ios::in)
+        {
+            open_mode = ios::in;
+        }
+        file.open(path, open_mode);
+        if (file.is_open())
+        {
+            while (getline(file, buf))
+            {
+                text += buf;
+                text += "\n";
+            }
+            this->CloseFile();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool CreateFile() {
+        if (open_mode != ios::out)
+        {
+            open_mode = ios::out;
+        }
+        file.open(path, open_mode);
+        return (file.is_open()) ? true : false;
+    }
+    void CloseFile() {
+        this->file.close();
+    }
+}; 
+
+int mystrlen(string str)
+{
+    int i = 0;
+    int counter = 0;
+    while (str[i] != '\0')
+    {
+        counter++;
+        i++;
+    }
+    return counter;
+}
+
+int main()
+{
+    File* file = new File("text.txt");
+    file->Write("If he's a serial killer, then whats the worst\nThat can happen to a girl who's already hurt\nI'm already hurt");
+    string text;
+    int counter = 0;
+    file->Load(text);
+    for (size_t i = 0; i < mystrlen(text); i++)
+    {
+        if (text[i] == ' ' || text[i] == '\t' || text[i] == '\n')
+        {
+            counter++;
+        }
+    }
+    cout << text << endl << endl;
+    cout << "Number of words: " << counter << endl;
+    return 0;
+}
